143546286fa7ac0e032f077386c5259910836959
ronniestack <ronniecalleda08@gmail.com>
2025-09-18 18:22:21 +0800

    Improve indentions for better readability

--- resources/views/backend/service/index.blade.php
+++ resources/views/backend/service/index.blade.php
@@ -18,39 +18,41 @@
 
 @section('content')
     <div class="">
-        <!-- Content Header (Page header) -->
-        <!-- Content Header (Page header) -->
-
-        <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
+
+                <!-- Error toast -->
                 @if (count($errors) > 0)
-                <div class="alert alert-dismissable alert-danger mt-3">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <strong>Whoops!</strong> There were some problems with your input.<br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            @if (session('success'))
-                <div class="alert alert-success alert-dismissable">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <strong>{{ session('success') }}</strong>
-                </div>
-            @endif
+                    <div class="alert alert-dismissable alert-danger mt-3">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <strong>Whoops!</strong> There were some problems with your input.<br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                
+                <!-- Success toast -->
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <strong>{{ session('success') }}</strong>
+                    </div>
+                @endif
+
+                <!-- Main content of the page -->
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card py-2 px-2">
-
                             <div class="card-body p-0">
-                                <table id="myTable" class="table table-striped projects ">
+
+                                <table id="myTable" class="table table-striped projects">
                                     <thead>
                                         <tr>
                                             <th style="width: 1%">
@@ -65,9 +67,11 @@
                                             <th style="width: 10%">
                                                 Category
                                             </th>
+
                                             {{-- <th style="width: 10%">
                                                 Featured
                                             </th> --}}
+
                                             <th style="width: 10%" class="text-center">
                                                 Status
                                             </th>
@@ -100,9 +104,9 @@
                                                     @endif
                                                 </td>
                                                 <td>
-
                                                     {{ $service->category->title ?? 'NA' }}
                                                 </td>
+
                                                 {{-- <td>
                                                     @if ($service->featured)
                                                         Yes
@@ -115,29 +119,26 @@
                                                     @if ($service->status)
                                                         <span class="badge badge-success">Active</span>
                                                     @else
-                                                        <span class="badge badge-danger">Pending</span>
+                                                        <span class="badge badge-danger">Inactive</span>
                                                     @endif
                                                 </td>
                                                 <td class="project-actions text-right d-flex justify-content-between">
                                                     <div>
                                                         <a class="btn btn-info btn-sm"
                                                             href="{{ route('service.edit', $service->id) }}">
-                                                            <i class="fas fa-pencil-alt">
-                                                            </i>
-                                                            Edit
+                                                            <i class="fas fa-pencil-alt"></i> Edit
                                                         </a>
                                                     </div>
                                                     <div>
                                                         <form action="{{ route('service.destroy', $service->id) }}"
                                                             method="post">
+
                                                             @csrf
                                                             @method('delete')
                                                             <button
-                                                                onclick="return confirm('Are you sure you want to delete this item?');"
+                                                                onclick="return confirm('Are you sure you?');"
                                                                 type="submit" class="btn btn-danger btn-sm">
-                                                                <i class="fas fa-trash">
-                                                                </i>
-                                                                Trash
+                                                                <i class="fas fa-trash"></i> Trash
                                                             </button>
                                                         </form>
                                                     </div>
@@ -148,14 +149,11 @@
                                 </table>
 
                             </div>
-                            <!-- /.card-body -->
                         </div>
                     </div>
-                    <!-- /.col -->
-
                 </div>
-                <!-- /.row -->
-            </div><!-- /.container-fluid -->
+
+            </div>
         </section>
     </div>
 @stop
@@ -165,8 +163,7 @@
 @stop
 
 @section('js')
-
-    {{-- hide notifcation --}}
+    <!-- Hide notification -->
     <script>
         $(document).ready(function() {
             $(".alert").delay(6000).slideUp(300);
@@ -178,46 +175,43 @@
             $('#myTable').DataTable({
                 responsive: true
             });
-
         });
     </script>
 
-
-    {{-- Sucess and error notification alert --}}
+    <!-- Sucess and error toast alert -->
     <script>
         $(document).ready(function() {
-        // show error message
-        @if ($errors->any())
-            //var errorMessage = @json($errors->any()); // Get the first validation error message
-            var Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 5500
-            });
+            // Show error message
+            @if ($errors->any())
+                // var errorMessage = @json($errors->any()); // Get the first validation error message
+                var Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 5500
+                });
 
-            Toast.fire({
-                icon: 'error',
-                title: 'There are form validation errors. Please fix them.'
-            });
-        @endif
+                Toast.fire({
+                    icon: 'error',
+                    title: 'There are form validation errors. Please fix them.'
+                });
+            @endif
 
-        // success message
-        @if (session('success'))
-            var successMessage = @json(session('success')); // Get the first sucess message
-            var Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 5500
-            });
+            // Show success message
+            @if (session('success'))
+                var successMessage = @json(session('success')); // Get the first sucess message
+                var Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 5500
+                });
 
-            Toast.fire({
-                icon: 'success',
-                title: successMessage
-            });
-        @endif
-
+                Toast.fire({
+                    icon: 'success',
+                    title: successMessage
+                });
+            @endif
         });
     </script>
 @endsection
