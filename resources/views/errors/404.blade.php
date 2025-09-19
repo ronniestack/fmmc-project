9198caecba65293a442378517891c63eec0f67cf
ronniestack <ronniecalleda08@gmail.com>
2025-09-19 13:55:47 +0800

    improve indentions

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -8,7 +7,6 @@
     <title>{{ env('APP_NAME') }} - 404 </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 
 <body>
     <div class="d-flex align-items-center justify-content-center vh-100">
@@ -22,6 +20,4 @@
         </div>
     </div>
 </body>
-
-
 </html>
